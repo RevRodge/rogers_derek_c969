b7daf3fab6b9b0be7ab29464eb4d21457af0f50e
revrodge <derekrogers007@gmail.com>
2025-12-29 15:55:58 -0700

    created and wired CustomerForm

---
+++ SchedulingApp/Forms/CustomerForm.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace SchedulingApp.Forms
+{
+    partial class CustomerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        private System.Windows.Forms.Label lblCustomerName;
+        private System.Windows.Forms.Label lblAddress1;
+        private System.Windows.Forms.Label lblAddress2;
+        private System.Windows.Forms.Label lblCityId;
+        private System.Windows.Forms.Label lblPostalCode;
+        private System.Windows.Forms.Label lblPhone;
+
+        private System.Windows.Forms.TextBox txtCustomerName;
+        private System.Windows.Forms.TextBox txtAddress1;
+        private System.Windows.Forms.TextBox txtAddress2;
+        private System.Windows.Forms.TextBox txtCityId;
+        private System.Windows.Forms.TextBox txtPostalCode;
+        private System.Windows.Forms.TextBox txtPhone;
+
+        private System.Windows.Forms.CheckBox chkActive;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCustomerName = new System.Windows.Forms.Label();
+            this.lblAddress1 = new System.Windows.Forms.Label();
+            this.lblAddress2 = new System.Windows.Forms.Label();
+            this.lblCityId = new System.Windows.Forms.Label();
+            this.lblPostalCode = new System.Windows.Forms.Label();
+            this.lblPhone = new System.Windows.Forms.Label();
+
+            this.txtCustomerName = new System.Windows.Forms.TextBox();
+            this.txtAddress1 = new System.Windows.Forms.TextBox();
+            this.txtAddress2 = new System.Windows.Forms.TextBox();
+            this.txtCityId = new System.Windows.Forms.TextBox();
+            this.txtPostalCode = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+
+            this.chkActive = new System.Windows.Forms.CheckBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+
+            this.SuspendLayout();
+
+            // lblCustomerName
+            this.lblCustomerName.AutoSize = true;
+            this.lblCustomerName.Location = new System.Drawing.Point(20, 10);
+            this.lblCustomerName.Name = "lblCustomerName";
+            this.lblCustomerName.Text = "Customer Name";
+
+            // txtCustomerName
+            this.txtCustomerName.Location = new System.Drawing.Point(20, 30);
+            this.txtCustomerName.Name = "txtCustomerName";
+            this.txtCustomerName.Size = new System.Drawing.Size(300, 20);
+            this.txtCustomerName.TabIndex = 0;
+
+            // lblAddress1
+            this.lblAddress1.AutoSize = true;
+            this.lblAddress1.Location = new System.Drawing.Point(20, 60);
+            this.lblAddress1.Name = "lblAddress1";
+            this.lblAddress1.Text = "Address Line 1";
+
+            // txtAddress1
+            this.txtAddress1.Location = new System.Drawing.Point(20, 80);
+            this.txtAddress1.Name = "txtAddress1";
+            this.txtAddress1.Size = new System.Drawing.Size(300, 20);
+            this.txtAddress1.TabIndex = 1;
+
+            // lblAddress2
+            this.lblAddress2.AutoSize = true;
+            this.lblAddress2.Location = new System.Drawing.Point(20, 110);
+            this.lblAddress2.Name = "lblAddress2";
+            this.lblAddress2.Text = "Address Line 2";
+
+            // txtAddress2
+            this.txtAddress2.Location = new System.Drawing.Point(20, 130);
+            this.txtAddress2.Name = "txtAddress2";
+            this.txtAddress2.Size = new System.Drawing.Size(300, 20);
+            this.txtAddress2.TabIndex = 2;
+
+            // lblCityId
+            this.lblCityId.AutoSize = true;
+            this.lblCityId.Location = new System.Drawing.Point(20, 160);
+            this.lblCityId.Name = "lblCityId";
+            this.lblCityId.Text = "City ID";
+
+            // lblPostalCode
+            this.lblPostalCode.AutoSize = true;
+            this.lblPostalCode.Location = new System.Drawing.Point(160, 160);
+            this.lblPostalCode.Name = "lblPostalCode";
+            this.lblPostalCode.Text = "Postal Code";
+
+            // txtCityId
+            this.txtCityId.Location = new System.Drawing.Point(20, 180);
+            this.txtCityId.Name = "txtCityId";
+            this.txtCityId.Size = new System.Drawing.Size(120, 20);
+            this.txtCityId.TabIndex = 3;
+
+            // txtPostalCode
+            this.txtPostalCode.Location = new System.Drawing.Point(160, 180);
+            this.txtPostalCode.Name = "txtPostalCode";
+            this.txtPostalCode.Size = new System.Drawing.Size(160, 20);
+            this.txtPostalCode.TabIndex = 4;
+
+            // lblPhone
+            this.lblPhone.AutoSize = true;
+            this.lblPhone.Location = new System.Drawing.Point(20, 210);
+            this.lblPhone.Name = "lblPhone";
+            this.lblPhone.Text = "Phone";
+
+            // txtPhone
+            this.txtPhone.Location = new System.Drawing.Point(20, 230);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(300, 20);
+            this.txtPhone.TabIndex = 5;
+
+            // chkActive (moved down so it doesn't overlap City/Postal row)
+            this.chkActive.AutoSize = true;
+            this.chkActive.Location = new System.Drawing.Point(20, 260);
+            this.chkActive.Name = "chkActive";
+            this.chkActive.Size = new System.Drawing.Size(56, 17);
+            this.chkActive.TabIndex = 6;
+            this.chkActive.Text = "Active";
+            this.chkActive.UseVisualStyleBackColor = true;
+
+            // btnSave (moved down so it doesn't overlap Phone textbox)
+            this.btnSave.Location = new System.Drawing.Point(160, 290);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 28);
+            this.btnSave.TabIndex = 7;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+
+            // btnCancel
+            this.btnCancel.Location = new System.Drawing.Point(245, 290);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 28);
+            this.btnCancel.TabIndex = 8;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+
+            // CustomerForm
+            this.ClientSize = new System.Drawing.Size(350, 335);
+            this.Controls.Add(this.lblCustomerName);
+            this.Controls.Add(this.txtCustomerName);
+
+            this.Controls.Add(this.lblAddress1);
+            this.Controls.Add(this.txtAddress1);
+
+            this.Controls.Add(this.lblAddress2);
+            this.Controls.Add(this.txtAddress2);
+
+            this.Controls.Add(this.lblCityId);
+            this.Controls.Add(this.txtCityId);
+
+            this.Controls.Add(this.lblPostalCode);
+            this.Controls.Add(this.txtPostalCode);
+
+            this.Controls.Add(this.lblPhone);
+            this.Controls.Add(this.txtPhone);
+
+            this.Controls.Add(this.chkActive);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnCancel);
+
+            this.Name = "CustomerForm";
+            this.Text = "Customer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ SchedulingApp/Forms/CustomerForm.cs
@@ -0,0 +1,109 @@
+﻿using SchedulingApp.Models;
+using SchedulingApp.Utilities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SchedulingApp.Forms
+{
+    public partial class CustomerForm : Form
+    {
+        public Customer Customer { get; private set; }
+        public CustomerForm()
+        {
+            InitializeComponent();
+
+            Text = "Add Customer";
+            chkActive.Checked = true;
+
+            btnSave.Click += btnSave_Click;
+            btnCancel.Click += btnCancel_Click;
+        }
+
+        public CustomerForm(Customer existingCustomer)
+        {
+            InitializeComponent();
+
+            Text = "Modify Customer";
+            btnSave.Click += btnSave_Click;
+            btnCancel.Click += btnCancel_Click;
+
+            if (existingCustomer == null)
+                throw new ArgumentNullException(nameof(existingCustomer));
+
+            // clone values into a working copy to not mutate the grid object
+            Customer = new Customer
+            {
+                CustomerId = existingCustomer.CustomerId,
+                CustomerName = existingCustomer.CustomerName,
+                AddressId = existingCustomer.AddressId,
+                Active = existingCustomer.Active,
+                AddressLine1 = existingCustomer.AddressLine1,
+                AddressLine2 = existingCustomer.AddressLine2,
+                CityId = existingCustomer.CityId,
+                PostalCode = existingCustomer.PostalCode,
+                Phone = existingCustomer.Phone,
+                CityName = existingCustomer.CityName,
+                CountryName = existingCustomer.CountryName
+            };
+
+            // Fill UI fields
+            txtCustomerName.Text = Customer.CustomerName;
+            txtAddress1.Text = Customer.AddressLine1;
+            txtAddress2.Text = Customer.AddressLine2;
+            txtCityId.Text = Customer.CityId.ToString();
+            txtPostalCode.Text = Customer.PostalCode;
+            txtPhone.Text = Customer.Phone;
+            chkActive.Checked = Customer.Active;
+        }
+
+        // ------------------ Buttons ------------------
+
+        //commits edits to a customer
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            // If Customer is null, switches to Add customer instead
+            if (Customer == null)
+                Customer = new Customer();
+
+            Customer.CustomerName = txtCustomerName.Text.Trim();
+            Customer.AddressLine1 = txtAddress1.Text.Trim();
+            Customer.AddressLine2 = txtAddress2.Text.Trim();
+            Customer.PostalCode = txtPostalCode.Text.Trim();
+            Customer.Phone = txtPhone.Text.Trim();
+            Customer.Active = chkActive.Checked;
+
+            // CityId must parse to int
+            if (!int.TryParse(txtCityId.Text.Trim(), out int cityId) || cityId <= 0)
+            {
+                MessageBox.Show("CityId must be a valid positive number.", "Validation",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            Customer.CityId = cityId;
+
+            // validate with The Validator(TM)
+            if (!Validator.TryValidateCustomer(Customer, out string error))
+            {
+                MessageBox.Show(error, "Validation", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+
+        //closes out and does not save any changes or additions
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            Close();
+        }
+    }
+}
