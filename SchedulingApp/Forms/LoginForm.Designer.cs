44a94d9045b280a1a133bee721df1b86ee0df3a0
revrodge <derekrogers007@gmail.com>
2025-12-28 17:48:19 -0700

    added form elements and validation

--- SchedulingApp/Forms/LoginForm.Designer.cs
+++ SchedulingApp/Forms/LoginForm.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace SchedulingApp
+﻿namespace SchedulingApp.Forms
 {
     partial class LoginForm
     {
@@ -6,6 +6,14 @@
         /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
+
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblZone;
 
         /// <summary>
         /// Clean up any resources being used.
@@ -19,22 +27,89 @@
             }
             base.Dispose(disposing);
         }
-
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblZone = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Location = new System.Drawing.Point(40, 30);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(58, 13);
+            this.lblUsername.Text = "Username";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(40, 70);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(53, 13);
+            this.lblPassword.Text = "Password";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(120, 27);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(180, 20);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(120, 67);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(180, 20);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(120, 110);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(80, 30);
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(220, 110);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(80, 30);
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblZone
+            // 
+            this.lblZone.AutoSize = true;
+            this.lblZone.Location = new System.Drawing.Point(40, 160);
+            this.lblZone.Name = "lblZone";
+            this.lblZone.Size = new System.Drawing.Size(0, 13);
+            // 
+            // LoginForm
+            // 
+            this.AcceptButton = this.btnLogin;
+            this.ClientSize = new System.Drawing.Size(350, 200);
+            this.Controls.Add(this.lblZone);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblUsername);
+            this.Name = "LoginForm";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
-        #endregion
     }
 }
 
--- SchedulingApp/Forms/LoginForm.cs
+++ SchedulingApp/Forms/LoginForm.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using SchedulingApp.Data;
+using SchedulingApp.Models;
+using SchedulingApp.Utilities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,13 +11,135 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace SchedulingApp
+namespace SchedulingApp.Forms
 {
     public partial class LoginForm : Form
     {
+        private readonly UserRepo _userRepo = new UserRepo();
+        private readonly ApptRepo _apptRepo = new ApptRepo();
         public LoginForm()
         {
             InitializeComponent();
+            
+            //displays local timezone on login screen
+            //cutting if it doesn't work
+            try
+            {
+                if (lblZone != null)
+                {
+                    lblZone.Text = TimeZoneInfo.Local.DisplayName;
+                }
+            }
+            catch
+            {
+                // Ignore if anything goes wrong
+            }
+
+            // allow Enters to trigger login
+            AcceptButton = btnLogin;
         }
+
+        //easy one first, closes form
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text.Trim();
+            string password = txtPassword.Text.Trim();
+
+            if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
+            {
+                MessageBox.Show(GetText("LoginMissingFields"), GetText("LoginTitle"),
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            User user;
+            try
+            {
+                user = _userRepo.ValidateUser(username, password);
+            }
+            catch (Exception ex)
+            {
+                // If DB is down / credentials bad / etc.
+                Logger.LogError("Login DB error", ex);
+                MessageBox.Show(GetText("LoginDbError"), GetText("LoginTitle"),
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (user == null)
+            {
+                MessageBox.Show(GetText("LoginInvalid"), GetText("LoginTitle"),
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Successful login: log it to file
+            Logger.LogLogin(user.UserName);
+
+            // checks for upcoming appointment within 15 minutes (rubric requirement)
+            try
+            {
+                DateTime nowUtc = DateTime.UtcNow;
+                DateTime soonUtc = nowUtc.AddMinutes(15);
+
+                var upcoming = _apptRepo.GetForUserBetween(user.UserId, nowUtc, soonUtc);
+
+                if (upcoming.Any())
+                {
+                    // Pick the earliest one
+                    var next = upcoming.OrderBy(a => a.StartUtc).First();
+
+                    DateTime startLocal = TimeZoneHelper.UtcToLocal(next.StartUtc);
+
+                    MessageBox.Show(
+                        string.Format(GetText("UpcomingApptYes"), startLocal),
+                        GetText("LoginTitle"),
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Information
+                    );
+                }
+                else
+                {
+                    MessageBox.Show(GetText("UpcomingApptNo"), GetText("LoginTitle"),
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Don't block login if the alert fails, but do log it
+                Logger.LogError("Upcoming appointment check failed", ex);
+            }
+
+            // Open main form and hide login
+            var main = new MainForm(user);
+            Hide();
+            main.FormClosed += (s, args) => Close();
+            main.Show();
+        }
+        private string GetText(string key)
+        {
+            switch (key)
+            {
+                case "LoginTitle": 
+                    return "Login";
+                case "LoginMissingFields": 
+                    return "Please enter a username and password.";
+                case "LoginInvalid": 
+                    return "Invalid username or password.";
+                case "LoginDbError": 
+                    return "Unable to connect to the database.";
+                case "UpcomingApptYes": 
+                    return "You have an appointment within 15 minutes. Start: {0}";
+                case "UpcomingApptNo": 
+                    return "You have no appointments within the next 15 minutes.";
+                default: return key;
+            }
+        }
+
     }
 }
